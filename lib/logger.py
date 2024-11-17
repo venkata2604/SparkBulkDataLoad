f3e1dfd456e4d02be2938eb8f2e3513a61963c48
venkata2604 <venkatasaiaavula@gmail.com>
2024-11-17 13:17:09 -0600

    Sync changes

--- lib/logger.py
+++ lib/logger.py
@@ -1,16 +1,16 @@
-class Log4J():
+class Log4j(object):
     def __init__(self, spark):
-        log4J = spark.__javm.orh.apache.log4j
-        self.logger = log4j.LogManager.getLogger("sdb1")
+        log4j = spark._jvm.org.apache.log4j
+        self.logger = log4j.LogManager.getLogger("sbdl")
 
     def warn(self, message):
         self.logger.warn(message)
 
-    def infor(self, message):
+    def info(self, message):
         self.logger.info(message)
 
     def error(self, message):
         self.logger.error(message)
 
-    def debug (self, message):
+    def debug(self, message):
         self.logger.debug(message)
